3d886db57f88f222d639ff5755e46a5847fc1296
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-06-10 13:25:33 +0100

    tables made

---
+++ db/bucket_list_tracker.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS countries;
+DROP TABLE IF EXISTS cities;
+DROP TABLE IF EXISTS attractions;
+
+CREATE TABLE countries (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    region VARCHAR(255)
+);
+
+CREATE TABLE cities (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    country_id INT REFERENCES countries(id) ON DELETE CASCADE
+);
+
+CREATE TABLE attractions (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    city_id INT REFERENCES cities(id) ON DELETE CASCADE,
+    date VARCHAR(255),
+    visited BOOLEAN
+);

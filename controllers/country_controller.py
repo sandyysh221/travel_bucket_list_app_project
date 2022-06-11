e2d18394badc3ac884ac5b5ed67edfdbf1a69665
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-06-11 23:16:12 +0100

    rough set up for webapp

--- controllers/country_controller.py
+++ controllers/country_controller.py
@@ -4,3 +4,9 @@ from models.country import Country
 import repositories.country_repository as country_repository
 
 countries_blueprint = Blueprint("country", __name__)
+
+
+@countries_blueprint.route("/countries")
+def countries():
+    countries = country_repository.select_all()
+    return render_template("countries/index.html", countries=countries)
